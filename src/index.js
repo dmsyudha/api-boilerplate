45a27b0a00602cac0f8f34ec82ed9412c7462396
divaryanm <dryanm2001@gmail.com>
2022-04-19 01:55:09 +0700

    coba commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
+    logger.info('test');
   });
 });
 
